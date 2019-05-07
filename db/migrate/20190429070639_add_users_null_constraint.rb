694dd150a34bccbd5dbeb8e797fe0b98eb79bc32
Кирилл Войтулевич <voytulevich.k@gmail.com>
2019-05-07 08:52:15 +0300

    bag

--- db/migrate/20190429070639_add_users_null_constraint.rb
+++ db/migrate/20190429070639_add_users_null_constraint.rb
@@ -1,9 +1,9 @@
 class AddUsersNullConstraint < ActiveRecord::Migration[5.2]
   def up
-    hange_column_null(:users, :name, false)
+    change_column_null(:users, :name, false)
   end
 
   def down
-    hange_column_null(:users, :name, true)
+    change_column_null(:users, :name, true)
   end
 end
