745c7129a069ff27a9064094de70c2095a7b6f2c
Кирилл Войтулевич <voytulevich.k@gmail.com>
2019-05-29 13:16:07 +0300

    mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
+      session[:user_id] = @user.id
       redirect_to tests_path
     else
       render :new
@@ -16,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email.downcase, :password, :password_confirmation)
   end
 
 end
