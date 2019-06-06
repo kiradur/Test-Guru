42b0cf774ef92932a5b401846d1dd6aa904d518c
Кирилл Войтулевич <voytulevich.k@gmail.com>
2019-06-06 13:12:58 +0300

    dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'nameless-meadow-56795.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    authentication: 'plain',
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
+    address:              'smtp.gmail.com',
+    port:                 587,
+    authentication:       'plain',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
